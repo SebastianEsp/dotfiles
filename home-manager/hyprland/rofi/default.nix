91dda733161344f244935788ce06963f8592d18f
SebastianEsp <sebastianesp@hotmail.com>
2024-11-14 15:23:33 +0100

    rofi styles

--- home-manager/hyprland/rofi/default.nix
+++ home-manager/hyprland/rofi/default.nix
@@ -4,5 +4,6 @@
         rofi-wayland
     ];
 
+    home.file.".config/rofi/styles".source = ./styles;
     home.file."rofilaunch.sh".source = ./rofilaunch.sh;
 }
--- home-manager/hyprland/rofi/rofilaunch.sh
+++ home-manager/hyprland/rofi/rofilaunch.sh
@@ -2,9 +2,9 @@
 
 #// set variables
 roconf="$HOME/.config/rofi/styles/style.rasi"
-$rofiScale=9
-$hypr_border="$(hyprctl -j getoption decoration:rounding | jq '.int')"
-$hypr_width="$(hyprctl -j getoption general:border_size | jq '.int')"
+rofiScale=9
+hypr_border="$(hyprctl -j getoption decoration:rounding | jq '.int')"
+hypr_width="$(hyprctl -j getoption general:border_size | jq '.int')"
 
 [[ "${rofiScale}" =~ ^[0-9]+$ ]] || rofiScale=10
 
