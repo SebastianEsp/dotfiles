14b6f52e2e41e0fa58613bd57163f48298a56314
SebastianEsp <sebastianesp@hotmail.com>
2025-02-09 13:33:53 +0100

    waybar assets

--- home-manager/hyprland/waybar/default.nix
+++ home-manager/hyprland/waybar/default.nix
@@ -4,6 +4,7 @@
         waybar
     ];
 
+    home.file.".config/waybar/assets".source = ./assets;
     home.file."playerctl.sh".source = ./modules/playerctl/playerctl.sh;
     home.file.".config/waybar/style.css".source = ./style.css;
     home.file.".config/waybar/config.jsonc".source = ./config.jsonc;
