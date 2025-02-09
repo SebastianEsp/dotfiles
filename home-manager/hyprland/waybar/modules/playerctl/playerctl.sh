aed93650f9618433341a5da292b7e3f2bd6bdb5c
SebastianEsp <sebastianesp@hotmail.com>
2025-02-09 18:40:57 +0100

    scrolling playerctl artist name and track

--- home-manager/hyprland/waybar/modules/playerctl/playerctl.sh
+++ home-manager/hyprland/waybar/modules/playerctl/playerctl.sh
@@ -10,15 +10,33 @@ while getopts ":p:" opt; do
   esac
 done
 
-while true; do
+count=0
 
+while true; do
 	while read -r playing position length name artist title arturl hpos hlen; do
 		# remove leaders
 		playing=${playing:1} position=${position:1} length=${length:1} name=${name:1}
 		artist=${artist:1} title=${title:1} arturl=${arturl:1} hpos=${hpos:1} hlen=${hlen:1}
 
 		# build line
-		line="${artist:+$artist ${title:+- }}${title:+$title }${hpos:+$hpos${hlen:+|}}$hlen"
+		#line="${artist:+$artist ${title:+- }}${title:+$title }${hpos:+$hpos${hlen:+|}}$hlen"
+
+        preview="${artist:+$artist ${title:+- }}${title:+$title }"
+
+        # Scrolling artist info
+        if [[ "${#preview}" -gt 23 ]]; then
+            line="${preview:$count:23}... ${hpos:+$hpos${hlen:+|}}$hlen"
+            #line="$count - ${#preview}"
+            if [[ $count -lt "${#preview}" ]]; then
+                count=$((count + 1))
+            fi
+
+            if [[ $count -eq $(("${#preview}" - 23)) ]]; then
+                count=0
+            fi
+        else
+            line="${artist:+$artist ${title:+- }}${title:+$title }${hpos:+$hpos${hlen:+|}}$hlen"
+        fi
 
 		# json escaping
 		line="${line//\"/\\\"}"
