871d50411301942032bbc54aec3da7b3ea09b1de
SebastianEsp <sebastianesp@hotmail.com>
2025-02-09 13:24:31 +0100

    waybar playerctl

--- home-manager/hyprland/waybar/modules/playerctl/playerctl.sh
+++ home-manager/hyprland/waybar/modules/playerctl/playerctl.sh
@@ -1,3 +1,51 @@
 #!/usr/bin/env bash
+# Credit https://gitlab.com/xPMo/dotfiles.cli/-/blob/dots/.local/lib/waybar/playerctl.sh
+exec 2>"$XDG_RUNTIME_DIR/waybar-playerctl.log"
+IFS=$'\n\t'
 
-echo "SUCCESS"
+while getopts ":p:" opt; do
+  case ${opt} in
+    p ) player=$OPTARG;;
+    \? ) echo "Usage: cmd [-p]";;
+  esac
+done
+
+while true; do
+
+	while read -r playing position length name artist title arturl hpos hlen; do
+		# remove leaders
+		playing=${playing:1} position=${position:1} length=${length:1} name=${name:1}
+		artist=${artist:1} title=${title:1} arturl=${arturl:1} hpos=${hpos:1} hlen=${hlen:1}
+
+		# build line
+		line="${artist:+$artist ${title:+- }}${title:+$title }${hpos:+$hpos${hlen:+|}}$hlen"
+
+		# json escaping
+		line="${line//\"/\\\"}"
+		((percentage = length ? (100 * (position % length)) / length : 0))
+		case $playing in
+		⏸️ | Paused) text=" $line" ;;
+		▶️ | Playing) text=" $line" ;;
+		*) text='<span foreground=\"#073642\">⏹</span>' ;;
+		esac
+
+		# exit if print fails
+		printf '{"text":"%s","class":"%s","percentage":%s}\n' \
+			"$text" "$percentage" "$percentage" || break 2
+
+	done < <(
+		# requires playerctl>=2.0
+		# Add non-space character ":" before each parameter to prevent 'read' from skipping over them
+		playerctl --follow metadata --player $player --format \
+			$':{{emoji(status)}}\t:{{position}}\t:{{mpris:length}}\t:{{playerName}}\t:{{markup_escape(artist)}}\t:{{markup_escape(title)}}\t:{{mpris:artUrl}}\t:{{duration(position)}}\t:{{duration(mpris:length)}}' &
+		echo $! >"$XDG_RUNTIME_DIR/waybar-playerctl.pid"
+	)
+
+	# no current players
+	# exit if print fails
+	echo '' || break
+	sleep 15
+
+done
+
+kill "$(<"$XDG_RUNTIME_DIR/waybar-playerctl.pid")"
