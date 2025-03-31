1c25649c710098948526b458bc1e597b3d7ad378
SebastianEsp <sebastianesp@hotmail.com>
2025-03-31 23:37:40 +0200

    sekirofpsunlock

--- pkgs/sekirofpsunlock/default.nix
+++ pkgs/sekirofpsunlock/default.nix
@@ -1,15 +1,21 @@
+{ pkgs ? import <nixpkgs> {} }:
+
 pkgs.stdenv.mkDerivation {
     name = "sekirofpsunlock";
 
     src = pkgs.fetchurl {
-        name = "hello_static";
         url = "https://github.com/Lahvuun/sekirofpsunlock/releases/download/v0.2.3/sekirofpsunlock-v0.2.3-x86_64-musl.tar.gz";
-        sha256 = "sha256:somE27ajbm0TtWv9tyeqTWDW3gbIs6xvlcFS9QS1ZJ0=";
+        sha256 = "sha256-BReGWCAMx8ZqURh6JdjEtpksA8KpCMwAi11LSjPAiBY=";
     };
 
-    phases = [ "installPhase" ];
+    phases = "unpackPhase unpackPhaseExport";
 
-    installPhase = ''
-        tar xf sekirofpsunlock-v0.2.3-x86_64-musl.tar.gz -C $out
+    unpackPhase = ":";
+
+    unpackPhaseExport = ''
+        cp $src sekirofpsunlock-v0.2.3-x86_64-musl.tar.gz
+        tar -xvzf sekirofpsunlock-v0.2.3-x86_64-musl.tar.gz
+        mkdir -p $out/bin
+        cp sekirofpsunlock $out/bin/
     '';
 }
